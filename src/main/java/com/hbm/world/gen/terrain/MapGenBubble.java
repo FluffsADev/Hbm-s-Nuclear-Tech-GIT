8b829b421e0c30f12b5c97034a66b39988888b29
FluffsADev <fluffiedlive@gmail.com>
2026-06-04 08:50:44 -0400

    Generation Bug Fix + lang

--- src/main/java/com/hbm/world/gen/terrain/MapGenBubble.java
+++ src/main/java/com/hbm/world/gen/terrain/MapGenBubble.java
@@ -16,10 +16,6 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 public class MapGenBubble extends MapGenBaseMeta {
 
-	/**
-	 * Generates oil bubbles, which are generally wider than a chunk, in a safe + cascadeless manner
-	 * Pretty much just an oblate sphere generator (dimensions: 3 x 1 x 3)
-	 */
 
 	private final int frequency;
 	public Random rand;
@@ -39,6 +35,7 @@ public class MapGenBubble extends MapGenBaseMeta {
 
 	public MapGenBubble(int frequency) {
 		this.frequency = frequency;
+		this.rand = new Random();
 	}
 
 	public void setSize(int minSize, int maxSize) {
@@ -51,12 +48,14 @@ public class MapGenBubble extends MapGenBaseMeta {
 	@Override
 	public void func_151538_a(World world, int offsetX, int offsetZ, int chunkX, int chunkZ, Block[] blocks) {
 
+		if(world == null || blocks == null || rand == null || block == null || replace == null) return;
+
 		int effecFreq = frequency;
 		BiomeGenBase biome = world.getBiomeGenForCoords(offsetX * 16, offsetZ * 16);
-		if(biome.temperature >= 2 && biome.rainfall < 0.1) effecFreq /= 3;
+		if(biome != null && biome.temperature >= 2 && biome.rainfall < 0.1) effecFreq /= 3;
 		if(effecFreq <= 0) effecFreq = 1;
 
-		if(rand.nextInt(effecFreq) == effecFreq - 1 && (canSpawn == null || canSpawn.test(biome))) {
+		if(rand.nextInt(effecFreq) == effecFreq - 1 && (canSpawn == null || biome == null || canSpawn.test(biome))) {
 			int xCoord = (chunkX - offsetX) * 16 + rand.nextInt(16);
 			int zCoord = (chunkZ - offsetZ) * 16 + rand.nextInt(16);
 
@@ -68,33 +67,36 @@ public class MapGenBubble extends MapGenBaseMeta {
 			int yMin = Math.max(1, MathHelper.floor_double(yCoord - radius));
 			int yMax = Math.min(127, MathHelper.ceiling_double_int(yCoord + radius));
 
-			for(int bx = 15; bx >= 0; bx--) // bx, bz is the coordinate of the block we're modifying, relative to the generating chunk origin
-			for(int bz = 15; bz >= 0; bz--)
-			for(int by = yMin; by < yMax; by++) {
-				int index = (bx * 16 + bz) * 256 + by;
+			for(int bx = 15; bx >= 0; bx--)
+				for(int bz = 15; bz >= 0; bz--)
+					for(int by = yMin; by < yMax; by++) {
+						int index = (bx * 16 + bz) * 256 + by;
 
-				if(blocks[index] == replace) {
-					// x, z are the coordinates relative to the target virtual chunk origin
-					int x = xCoord + bx;
-					int z = zCoord + bz;
-					int y = yCoord - by;
+						if(index < 0 || index >= blocks.length) continue;
 
-					double rSqr = x * x + z * z + y * y * 3;
-					if(fuzzy) rSqr -= rand.nextDouble() * radiusSqr / 3;
-					if(rSqr < radiusSqr) {
-						blocks[index] = block;
-						metas[index] = meta;
+						if(blocks[index] == replace) {
+							int x = xCoord + bx;
+							int z = zCoord + bz;
+							int y = yCoord - by;
+
+							double rSqr = x * x + z * z + y * y * 3;
+							if(fuzzy) rSqr -= rand.nextDouble() * radiusSqr / 3;
+							if(rSqr < radiusSqr) {
+								blocks[index] = block;
+								if(metas != null && index < metas.length) {
+									metas[index] = meta;
+								}
+							}
+						}
 					}
-				}
-			}
 
-			if(rand.nextInt(1) == 0) {
-				addSurfaceSpot(xCoord, zCoord, blocks);
-			}
+			addSurfaceSpot(xCoord, zCoord, blocks);
 		}
 	}
 
 	protected void addSurfaceSpot(int xCoord, int zCoord, Block[] blocks) {
+
+		if(blocks == null || rand == null) return;
 
 		int deadMetaCount = EnumDeadPlantType.values().length;
 		int spotCount = 150;
@@ -108,13 +110,15 @@ public class MapGenBubble extends MapGenBaseMeta {
 			int rz = offZ - zCoord;
 
 			if(rx >= 0 && rx < 16 && rz >= 0 && rz < 16) {
-				// find ground level
 				for(int y = 127; y >= 0; y--) {
 					int index = (rx * 16 + rz) * 256 + y;
 
+					if(index < 0 || index >= blocks.length) continue;
 					if(blocks[index] != null && blocks[index].isOpaqueCube()) {
 						for(int oy = 1; oy > -3; oy--) {
 							int subIndex = index + oy;
+
+							if(subIndex < 0 || subIndex >= blocks.length) continue;
 
 							int distSq = offX * offX + offZ * offZ;
 							boolean inner = distSq < (spotWidth / 2) * (spotWidth / 2);
@@ -122,16 +126,19 @@ public class MapGenBubble extends MapGenBaseMeta {
 							if(blocks[subIndex] == Blocks.grass || blocks[subIndex] == Blocks.dirt) {
 								blocks[subIndex] = inner ? ModBlocks.dirt_oily : ModBlocks.dirt_dead;
 
-								// this generation occurs BEFORE decoration, so we have no plants to modify
-								// so we'll instead just add some new ones right now
 								if(!inner && oy == 0 && rand.nextInt(20) == 0) {
-									blocks[subIndex + 1] = ModBlocks.plant_dead;
-									metas[subIndex + 1] = (byte)rand.nextInt(deadMetaCount);
+									int plantIndex = subIndex + 1;
+									if(plantIndex >= 0 && plantIndex < blocks.length) {
+										blocks[plantIndex] = ModBlocks.plant_dead;
+										if(metas != null && plantIndex < metas.length) {
+											metas[plantIndex] = (byte)rand.nextInt(deadMetaCount);
+										}
+									}
 								}
 
 								break;
 							} else if(blocks[subIndex] == Blocks.sand || blocks[subIndex] == ModBlocks.ore_oil_sand) {
-								if(metas[subIndex] == 1) {
+								if(metas != null && subIndex < metas.length && metas[subIndex] == 1) {
 									blocks[subIndex] = ModBlocks.sand_dirty_red;
 								} else {
 									blocks[subIndex] = ModBlocks.sand_dirty;
@@ -149,7 +156,6 @@ public class MapGenBubble extends MapGenBaseMeta {
 			}
 		}
 
-		// and now for the hole(tm)
 		for(int i = 1; i < 6; i++) {
 			ForgeDirection dir = ForgeDirection.getOrientation(i);
 			int x = dir.offsetX - xCoord;
@@ -160,6 +166,7 @@ public class MapGenBubble extends MapGenBaseMeta {
 
 				for(int y = 127; y >= 0; y--) {
 					int index = (x * 16 + z) * 256 + y;
+					if(index < 0 || index >= blocks.length) continue;
 					if(blocks[index] == null) continue;
 
 					if(blocks[index].getMaterial().isLiquid()) break;
@@ -167,7 +174,6 @@ public class MapGenBubble extends MapGenBaseMeta {
 					if(blocks[index].isOpaqueCube()) {
 						solids++;
 
-						// this approach might break a little when the surface has holes and uneveness but i don't care lmao
 						if(i > 1) {
 							blocks[index] = ModBlocks.stone_cracked;
 							if(solids >= 4) break;
